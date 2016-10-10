2301d10b6a6c53ef5906499a0212020b4161d3b2
adaiye <yj-baby@126.com>
2016-10-10 22:44:44 +0800

    T-Launch Android版的SplashScreen

--- android/app/src/main/java/com/earlymath/MainActivity.java
+++ android/app/src/main/java/com/earlymath/MainActivity.java
@@ -1,8 +1,25 @@
 package com.earlymath;
 
+import android.os.Bundle;
+import android.os.Handler;
+
 import com.facebook.react.ReactActivity;
 
+import static com.reactnativecomponent.splashscreen.RCTSplashScreen.UIAnimationFade;
+
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EMSplashScreen.openSplashScreen(this, true);   //open splashscreen fullscreen
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                EMSplashScreen.removeSplashScreen(null, UIAnimationFade, 300);
+            }
+        }, 2000);
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
