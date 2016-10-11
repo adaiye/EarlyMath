fcf4e8bea08486a4ed82313de41a256ba98a2e0a
adaiye <yj-baby@126.com>
2016-10-11 18:45:35 +0800

    T-Pic2Number 显示卡片并且可以点击

--- index.global.js
+++ index.global.js
@@ -4,6 +4,7 @@
  * @flow
  */
 
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,9 +13,19 @@ import {
   Image,
   View,
   TouchableHighlight,
+  Platform,
 } from 'react-native';
 
 var mainStyles = require('./mainStyle');
+
+class Card extends Component {
+    render() {
+        return (
+            <View style={styles.card2}>
+            </View>
+        );
+    }
+}
 
 class EarlyMath extends Component {
   constructor(props) {
@@ -37,8 +48,54 @@ class EarlyMath extends Component {
   __onPressLanguage() {
     console.log('language');
   }
+  __cardClicked(index: number): void {
+    this.setState({
+      language: this.state.language === 0 ? 1 : 0,
+    });
+  }
 
   render() {
+      var numberOfRow = 5, numberPerRow = 3;
+      let cards = [], rows = [];
+      let images = [
+          require('image!number_1'),
+          require('image!number_2'),
+          require('image!number_3'),
+          require('image!number_4'),
+          require('image!number_5'),
+          require('image!number_6'),
+          require('image!number_7'),
+          require('image!number_8'),
+          require('image!number_9'),
+      ]
+      for (var i = 0; i < numberPerRow * numberOfRow; i++) {
+          if (i < images.length) {
+              cards.push(
+                  <TouchableHighlight
+                      style={styles.card}
+                      key={'card'+i}
+                      onPress={this.__cardClicked.bind(this, i)} >
+                      <Image source={images[i]} style={styles.cardImage} />
+                  </TouchableHighlight>
+              );
+          } else {
+              cards.push(
+                  <TouchableHighlight style={styles.card}
+                      key={'card'+i}>
+                      <Image source={images[i%9]} style={styles.cardImage} />
+                  </TouchableHighlight>
+              );
+          }
+      }
+      for (var i = 0; i < numberOfRow; i++) {
+          var start = i * numberPerRow;
+          rows.push(
+              <View style={styles.row}
+                  key={'row'+i}>
+                  { cards.slice(start, start + numberPerRow) }
+              </View>
+          )
+      }
     return (
       <View style={mainStyles.mainContainer}>
 
@@ -58,16 +115,7 @@ class EarlyMath extends Component {
         </View>
 
         <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit index.android.js
-          </Text>
-          <Text style={styles.instructions}>
-            Double tap R on your keyboard to reload,{'\n'}
-            Shake or press menu button for dev menu
-          </Text>
+            {rows}
         </View>
 
       </View>
@@ -78,18 +126,41 @@ class EarlyMath extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    margin:10,
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  row: {
+      flex: 1,
+      flexDirection:'row',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  card: {
+      flex: 1,
+      margin:5,
+      justifyContent: 'center',
+      alignItems: 'stretch',
+      borderColor: '#F5CF8768',
+      borderWidth: 1,
+      borderRadius: 8,
+      ...Platform.select({
+        ios: {
+          shadowColor: '#F5CF87',
+          shadowOpacity: 0.2,
+          shadowOffset: {
+            width: 0,
+            height: 0.5,
+          },
+          shadowRadius: 2,
+        },
+      }),
+  },
+  cardImage: {
+      flex: 1,
+      width: null,
+      height: null,
+      resizeMode: 'stretch',
   },
 });
 
